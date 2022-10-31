943ecdc1f03e5e57c3b5aa816b28476ec17f030b
dibi babu <dibikannan@gmail.com>
2022-10-31 19:31:51 +0530

    single commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState } from "react";
+import { firebase } from "./Firebase/config";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/config.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANlCcmvpkrKijDPP9oCol3A3hFofSKZEg",
+  authDomain: "olx-clone-28e7d.firebaseapp.com",
+  projectId: "olx-clone-28e7d",
+  storageBucket: "olx-clone-28e7d.appspot.com",
+  messagingSenderId: "313003886047",
+  appId: "1:313003886047:web:4069837cf6be1cd877306a",
+  measurementId: "G-YRD56D2JTD"
+};
+
+export const firebase = initializeApp(firebaseConfig)
